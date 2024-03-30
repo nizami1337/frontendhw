94d61989f4689e6a555f4707d4f4aced6fc93870
x <x@x.com>
2024-03-30 11:45:19 +0400

    hw

--- index.js
+++ index.js
@@ -1,48 +1,58 @@
-let car = {
-    make: "Ford",
-    model: "Capri",
-    year: 1978,
-    __proto__: {
-        start(extraText) {
-            console.log(`${this.make} ${this.model} ise salindi. ${extraText}.`)
+// 1
+class Employee {
+    constructor(name, age, position, salary) {
+        this.name = name;
+        this.age = age;
+        this.position = position;
+        this.salary = salary;
+    }
+
+    promote(newPosition, newSalary) {
+        this.position = newPosition;
+        console.log(`${this.name} ${newPosition} vezifesine promote oldu. Maas artimi: ${newSalary - this.salary}`)
+        this.salary = newSalary;
+    }
+
+    birthday() {
+        this.age++;
+        console.log(`${this.name} artiq ${this.age} yasindadir`)
+    }
+
+    displayInfo() {
+        console.log(`Ad: ${this.name} | Yas: ${this.age} | Vezife: ${this.position} | Maas: ${this.salary}`)
+    }
+}
+
+emp = new Employee("Ruslan", 24, "Junior Network Engineer", 800)
+emp.displayInfo()
+emp.birthday()
+emp.promote("Senior Network Engineer", 1200)
+emp.displayInfo()
+
+// 2
+function secondMin(array) {
+    return array.sort()[1]
+}
+
+// 3
+// Example student
+// student = {
+//     name: "Omer",
+//     score: 95
+// }
+function highestScoreStudent(studentList) {
+    stud = {name: '', score: 0}
+    for (let i of studentList) {
+        if (i.score > stud.score) {
+            stud = i
         }
     }
+    return stud
 }
+let studentList = [{name: "Asd", score: 78}, {name: "Qwe", score: 90}, {name: "Zxc", score: 83}]
+console.log(highestScoreStudent(studentList))
 
-car.start("Traditional")
-
-let car2 = {
-    make: "Dodge",
-    model: "Charger",
-    year: 1971
+// 4
+function maxFrequency(array) {
+    return array.filter(i => i == Math.max(...array)).length
 }
-
-// BIND
-let car2_start_bind = car.start.bind(car2)
-car2_start_bind("Bind")
-
-// CALL
-car.start.call(car2, "Call")
-
-// APPLY
-car.start.apply(car2, ["Apply"])
-
-
-
-let prototypeForCars = {
-    info() {
-        console.log(`Car model ${this.model} was made by ${this.make} in ${this.year}`)
-    }
-}
-
-let car3 = {
-    make: "Plymouth",
-    model: "GTX",
-    year: 1972,
-    __proto__: prototypeForCars // method 1
-}
-
-// method 2
-// car3.__proto__ = prototypeForCars;
-
-car3.info()
